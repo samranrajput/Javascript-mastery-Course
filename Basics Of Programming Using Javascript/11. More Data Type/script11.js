34fc6c2f18b9a4da0296ff0624887240d2dd8b23
Comsoft-System <systemcomsoft@gmail.com>
2025-07-28 07:12:44 -1200

    forth commit

---
+++ Basics Of Programming Using Javascript/11. More Data Type/script11.js
@@ -0,0 +1,16 @@
+// undefined
+// let firstName;
+// console.log(typeof firstName)
+// firstName = "Samran"
+// console.log(typeof firstName)
+
+// null
+// let nullVar = null
+// console.log(nullVar)
+// console.log(typeof nullVar) // Bug
+
+// BigInt
+
+let numbers = "123456789"
+let intNumbers = BigInt(numbers)
+console.log(typeof intNumbers)
